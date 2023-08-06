b1bf3417a0f546e1892812ae21e7c23cbdd069a1
Falak <46991207+falakj@users.noreply.github.com>
2023-08-06 23:25:44 +0530

    CORS

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "Access-Control-Allow-Origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"  }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
