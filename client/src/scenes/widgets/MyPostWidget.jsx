dca7a468ebe1a03dbc0fdfac315688d1558c692b
Falak <46991207+falakj@users.noreply.github.com>
2023-08-06 22:57:03 +0530

    Update

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -29,7 +29,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`http://localhost:3001/posts`, {
+    const response = await fetch(`https://falakz-socio-social-media-responsive-deploy.vercel.app/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
